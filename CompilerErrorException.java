32a730058f41cba81479525a0e0ffb8184448c15
hubert007 <linwei9111@gmail.com>
2014-08-31 20:17:25 -0700

    Create CompilerErrorException.java

---
+++ CompilerErrorException.java
@@ -0,0 +1,10 @@
+package cop5555fa13;
+ 
+public class CompilerErrorException extends Exception {
+ 
+public CompilerErrorException(String string) {
+	// TODO Auto-generated constructor stub
+	super(string);
+}
+ 
+}
